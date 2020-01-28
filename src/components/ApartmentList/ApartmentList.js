5ac14874e0ee17fff97859ac7be201bc8545420f
anovikova <ptica-govorun@inbox.ru>
2020-01-28 05:12:51 +0300

    Промежуточный коммит

--- src/components/ApartmentList/ApartmentList.js
+++ src/components/ApartmentList/ApartmentList.js
@@ -9,19 +9,34 @@ export default class ApartmentList extends Component {
 
     componentDidMount() {
         console.log('componentDidMount');
-        this.resetTasks();
+        this.resetList();
     }
 
-    resetTasks = async () => {
+    resetList = async () => {
         const response = await axios.get('http://134.209.138.34/items');
         this.setState({ apartmentList: response.data });
         console.log(this.state.apartmentList);
     };
 
     render() {
+        const { apartmentList } = this.state;
+        const title = apartmentList.length > 0 && apartmentList[0].title;
+        const id = apartmentList.length > 0 && apartmentList[0].id;
+        const address = apartmentList.length > 0 && apartmentList[0].address;
+        const price = apartmentList.length > 0 && apartmentList[0].price;
+        const previewImage = apartmentList.length > 0 && apartmentList[0].previewImage;
+
         return (
             <div>
               Список аппартаментов
+                <br/>
+                <ul>
+                    <li>{title}</li>
+                    <li>{id}</li>
+                    <li>{address}</li>
+                    <li>{price}</li>
+                    <li><img src={previewImage}/></li>
+                </ul>
             </div>
         );
     }
