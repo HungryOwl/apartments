f0946becb6f892f45fbb6959f011d63d0a8ad093
anovikova <ptica-govorun@inbox.ru>
2020-01-29 15:41:19 +0300

    Условия для отрисовки в card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,21 +2,27 @@ import React from 'react';
 
 const Card = (props) => {
     const { features } = props;
+    const previewImage = features.previewImage || 'https://via.placeholder.com/240x180';
 
     return (
         <article className="card" data-id={features.id}>
             <a className="card__link" href="#">
-                <img className="card__img" src={features.previewImage} width='240' height='180'/>
+                <img className="card__img" src={previewImage} width='240' height='180'/>
             </a>
             <div className="card__textblock">
-                <div className="card__title">{features.title}</div>
-                <p className="card__row">
-                    <span className="card__caption">адрес:</span>
-                    <span className="card__value">{features.address}</span></p>
-                <p className="card__row">
-                    <span className="card__caption">стоимость:</span>
-                    <span className="card__value">{features.price}</span>
-                </p>
+                {features.title && <div className="card__title">{features.title}</div>}
+                {
+                    features.address && <p className="card__row">
+                        <span className="card__caption">адрес:</span>
+                        <span className="card__value">{features.address}</span>
+                    </p>
+                }
+                {
+                    features.price && <p className="card__row">
+                        <span className="card__caption">стоимость:</span>
+                        <span className="card__value">{features.price}</span>
+                    </p>
+                }
                 <a className="button button--details" href="#">Подробнее</a>
             </div>
         </article>
