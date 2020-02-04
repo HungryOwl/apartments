fdb9a74976d13c840e48d189146301d23e917fbd
anovikova <ptica-govorun@inbox.ru>
2020-02-04 22:49:23 +0300

    Переделываем карточку в класс и убираем копипасту внутри кода

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,41 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from "react-router-dom";
 
-const Card = ({ features }) => {
-    if (!features) {
-        return;
+export default class Card extends Component {
+    constructor(props) {
+        super(props);
     }
 
-    const previewImage = features.previewImage || 'https://via.placeholder.com/240x180';
+    renderRow(caption, value) {
+        return (
+            value &&
+            <p className="card__row">
+                <span className="card__caption">{caption}</span>
+                <span className="card__value">{value}</span>
+            </p>
+        )
+    }
 
-    return (
-        <article className="card">
-            <Link className="card__link" to={`/apartment/${features.id}`}>
-                <img className="card__img" src={previewImage} width='240' height='180'/>
-            </Link>
+    render() {
+        if (!this.props) {
+            return;
+        }
 
-            <div className="card__textblock">
-                <div className="card__text-wrapper">
-                    {features.title && <div className="card__title">{features.title}</div>}
-                    {
-                        features.address &&
-                        <p className="card__row">
-                            <span className="card__caption">адрес:</span>
-                            <span className="card__value">{features.address}</span>
-                        </p>
-                    }
-                    {
-                        features.price &&
-                        <p className="card__row">
-                            <span className="card__caption">стоимость:</span>
-                            <span className="card__value">{features.price}</span>
-                        </p>
-                    }
+        const { features } = this.props;
+        const previewImage = features.previewImage || 'https://via.placeholder.com/240x180';
+
+        return (
+            <article className="card">
+                <Link className="card__link" to={`/apartment/${features.id}`}>
+                    <img className="card__img" src={previewImage} width='240' height='180'/>
+                </Link>
+
+                <div className="card__textblock">
+                    <div className="card__text-wrapper">
+                        {features.title && <div className="card__title">{features.title}</div>}
+                        {this.renderRow('адрес:', features.address)}
+                        {this.renderRow('стоимость:', features.price)}
+                    </div>
+                    <Link className="card__button button button--details" to={`/apartment/${features.id}`}>Подробнее</Link>
                 </div>
-                <Link className="card__button button button--details" to={`/apartment/${features.id}`}>Подробнее</Link>
-            </div>
-        </article>
-    )
-};
+            </article>
+        )
+    }
+}
 
-export default Card;
+
+
+
+
+
+
+
